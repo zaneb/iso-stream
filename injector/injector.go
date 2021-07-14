7000eba994be3befd802be60508d76290d820cb7
Zane Bitter <zbitter@redhat.com>
2021-07-14 16:30:10 -0400

    Calculate size immediately
    Determine when the object is created whether the ignition area is large
    enough to contain the config.
    
    This greatly simplifies the code, but requires that the ISOReader be an
    io.ReadSeeker. This works for files but may not for streaming the ISO
    input.

--- injector/injector.go
+++ injector/injector.go
@@ -15,94 +15,75 @@ import (
 type RHCOSStreamReader struct {
 	// underlying reader for the actual iso
 	isoReader io.Reader
-	// reader configured to record the ignition info
-	infoReader io.Reader
 	// reader to use for the customized content
 	contentReader io.Reader
 
 	// ignition file info
-	haveIgnitionInfo   bool
-	ignitionInfo       *bytes.Buffer
-	ignitionAreaStart  int64
-	ignitionAreaLength int64
+	ignitionAreaStart  uint64
+	ignitionAreaLength uint64
 
 	// compressed ignition archive bytes reader
-	ignition       io.ReadSeeker
-	ignitionLength int64
+	ignition io.ReadSeeker
 }
 
 // header is inclusive of these bytes
 const headerStart = 32744
-const headerEnd = 32767
+const headerLen = 24
 const coreISOMagic = "coreiso+"
 
-func NewRHCOSStreamReader(isoReader io.Reader, ignitionContent string) (*RHCOSStreamReader, error) {
+func NewRHCOSStreamReader(isoReader io.ReadSeeker, ignitionContent string) (*RHCOSStreamReader, error) {
 	ignitionBytes, err := ignitionImageArchive(ignitionContent)
 	if err != nil {
 		return nil, errors.Wrap(err, "Failed to create compressed ignition archive")
 	}
 
-	sr := &RHCOSStreamReader{
-		isoReader:      isoReader,
-		ignitionInfo:   new(bytes.Buffer),
-		ignition:       bytes.NewReader(ignitionBytes),
-		ignitionLength: int64(len(ignitionBytes)),
+	areaStart, areaLength, err := CoreOSIgnitionArea(isoReader)
+	if err != nil {
+		return nil, err
 	}
 
-	// set up limit reader for the space before the header
-	b4HeaderReader := io.LimitReader(isoReader, headerStart)
+	ignitionLength := uint64(len(ignitionBytes))
+	if areaLength < ignitionLength {
+		return nil, errors.New(fmt.Sprintf("ignition length (%d) exceeds embed area size (%d)", ignitionLength, areaLength))
+	}
 
-	// set up a tee reader to write to the ignition info using a bytes buffer
-	ignitionInfoReader := io.TeeReader(io.LimitReader(isoReader, 24), sr.ignitionInfo)
-
-	sr.infoReader = io.MultiReader(b4HeaderReader, ignitionInfoReader)
+	sr := &RHCOSStreamReader{
+		isoReader:          isoReader,
+		ignition:           bytes.NewReader(ignitionBytes),
+		ignitionAreaStart:  areaStart,
+		ignitionAreaLength: areaLength,
+	}
 
 	return sr, nil
 }
 
-func (r *RHCOSStreamReader) transformIgnitionInfo() error {
-	infoBytes := make([]byte, 24)
-	n, err := r.ignitionInfo.Read(infoBytes)
-	if err != nil {
-		return errors.Wrap(err, "failed to read embed area data from buffer")
+func CoreOSIgnitionArea(isoReader io.ReadSeeker) (start, length uint64, err error) {
+	if _, err := isoReader.Seek(headerStart, io.SeekStart); err != nil {
+		return 0, 0, err
 	}
-	if n != 24 {
-		return errors.New(fmt.Sprintf("incorrect embed info size, expected 24, got %d", n))
-	}
-
-	res := bytes.Compare(infoBytes[0:8], []byte(coreISOMagic))
-	if res != 0 {
-		return errors.New(fmt.Sprintf("Could not find magic string in object header (%s)", infoBytes[0:8]))
+	infoBytes := make([]byte, headerLen)
+	isoReader.Read(infoBytes)
+	if _, err := isoReader.Seek(0, io.SeekStart); err != nil {
+		return 0, 0, err
 	}
 
-	r.ignitionAreaStart = int64(binary.LittleEndian.Uint64(infoBytes[8:16]))
-	r.ignitionAreaLength = int64(binary.LittleEndian.Uint64(infoBytes[16:24]))
-	r.haveIgnitionInfo = true
-
-	if r.ignitionAreaLength < r.ignitionLength {
-		return errors.New(fmt.Sprintf("ignition length (%d) exceeds embed area size (%d)", r.ignitionLength, r.ignitionAreaLength))
+	if bytes.Compare(infoBytes[0:8], []byte(coreISOMagic)) != 0 {
+		return 0, 0, fmt.Errorf("Could not find magic string in object header (%s)", infoBytes[0:8])
 	}
 
-	return nil
+	start = binary.LittleEndian.Uint64(infoBytes[8:16])
+	length = binary.LittleEndian.Uint64(infoBytes[16:24])
+
+	return
 }
 
 func (r *RHCOSStreamReader) Read(p []byte) (int, error) {
-	var err error
-
-	if !r.haveIgnitionInfo {
-		n, err := r.infoReader.Read(p)
-		if err == io.EOF {
-			err = r.transformIgnitionInfo()
-		}
-		return n, err
-	}
-
 	if r.contentReader == nil {
-		// Set the offset to the distance to the ignition taking into account that we've already read the system area
 		ignitionRange := &overreader.Range{
 			Content: r.ignition,
-			Offset:  r.ignitionAreaStart - headerEnd - 1,
+			Offset:  int64(r.ignitionAreaStart),
 		}
+		var err error
 		r.contentReader, err = overreader.NewReader(r.isoReader, ignitionRange)
 		if err != nil {
 			return 0, errors.Wrapf(err, "failed to create overwrite reader")
